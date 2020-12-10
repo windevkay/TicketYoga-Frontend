a70d8c1a8af0c6916273980e2a2d1927f955cfb0
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2020-12-09 20:41:07 -0700

    setting up apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,20 @@
 import ReactDOM from "react-dom";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+import { Tickets } from "./sections";
+
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById("root"));
+const client = new ApolloClient({ uri: "/api" });
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Tickets title={"TicketYoga"} />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/sections/Tickets/index.tsx
@@ -0,0 +1,7 @@
+interface props {
+  title: string;
+}
+
+export const Tickets = ({ title }: props) => {
+  return <div>{title}</div>;
+};
